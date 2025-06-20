a214a76bd0ec1f19241d10433fcca4a67af3a7ee
Izumitempest <lilice308@gmail.com>
2025-06-20 17:47:48 +0100

    Update configuration to load environment variables and set default secret keys

--- odyss-backend/app/config.py
+++ odyss-backend/app/config.py
@@ -1,9 +1,11 @@
 # config.py
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'change_me')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'change_me_too')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'FromThe104')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/odyss_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_SIZE = 10
