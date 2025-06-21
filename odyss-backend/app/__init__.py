887f72f2c0564c0debb26868901e0f8c39dac0ee
Izumitempest <lilice308@gmail.com>
2025-06-21 16:12:12 +0100

    Register booking blueprints in the application factory

--- odyss-backend/app/__init__.py
+++ odyss-backend/app/__init__.py
@@ -5,6 +5,10 @@ from app.extensions import db, jwt, migrate, cors
 from app.auth.routes import auth_bp
 from app.users.routes import user_bp
 from app.trips.routes import trips_bp
+# from .api.v1.routes import api_bp
+from app.bookings.routes import bookings_bp
+from app.bookings.routes import booking_bp
+
 
 
 def create_app():
@@ -13,6 +17,14 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(trips_bp)
+    app.register_blueprint(bookings_bp)
+    app.register_blueprint(booking_bp)
+    
+
+
+    # Register API Blueprint
+    # app.register_blueprint(api_bp, url_prefix='/api/v1')
+
 
     # Register extensions
 
