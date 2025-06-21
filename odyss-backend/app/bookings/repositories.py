89ca75f0837990379e8568758a421376e799ad46
Izumitempest <lilice308@gmail.com>
2025-06-21 16:15:10 +0100

    Add function to retrieve passengers by trip ID from the database

--- odyss-backend/app/bookings/repositories.py
+++ odyss-backend/app/bookings/repositories.py
@@ -1,1 +1,10 @@
 # repositories.py
+from app.models.booking import Booking
+from app.extensions import db
+
+def get_passengers_by_trip(trip_id):
+    return (
+        db.session.query(Booking)
+        .filter(Booking.trip_id == trip_id)
+        .all()
+    )
