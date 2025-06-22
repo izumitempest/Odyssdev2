42c21965a123443d492fa66045937490921bcb5a
Izumitempest <lilice308@gmail.com>
2025-06-22 02:19:47 +0100

    Add JWT configuration parameters for enhanced security

--- odyss-backend/app/config.py
+++ odyss-backend/app/config.py
@@ -6,6 +6,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'FromThe104')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default')
+    JWT_HEADER_TYPE = "Bearer"
+    JWT_ALGORITHM = "HS256"
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/odyss_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_SIZE = 10
