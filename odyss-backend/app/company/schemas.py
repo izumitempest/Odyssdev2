c5f44a06f43044b6ddabe4d504ca88e7db966642
Izumitempest <lilice308@gmail.com>
2025-06-25 02:30:56 +0100

    Add company signup schema definition

---
+++ odyss-backend/app/company/schemas.py
@@ -0,0 +1,12 @@
+# # app/companies/schemas.py
+
+# from marshmallow import Schema, fields
+
+# class CompanySignupSchema(Schema):
+#     name = fields.String(required=True)
+#     email = fields.Email(required=True)
+#     password = fields.String(required=True)
+#     company_name = fields.String(required=True)
+#     company_email = fields.Email(required=True)
+#     company_cert = fields.String(required=True)  # base64
+#     access_code = fields.String(required=True)
